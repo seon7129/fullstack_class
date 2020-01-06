bc93236ae18f5db3aec6b4b4346e8ce0ca77f37f
seon7129 <seon7129@naver.com>
2020-01-06 17:02:12 +0900

    update example 3

---
+++ index.js
@@ -0,0 +1,18 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const _webPort = 80;//link:80
+global.app = express();
+app.use(cors());
+global.router = express.Router();
+global.path = __dirname;
+app.use(bodyParser.json());
+app.use("/", router);
+
+router.get("/", (req, res) => {
+    res.sendFile(`${path}/web-client/index.html`);
+});
+
+app.listen(_webPort, () => {
+    console.log(`web server running on ${_webPort}`);
+});
